12e4295fba44faa8e89250f4bb1147f267192110
AhmadAlketnani <ahmadalktnany383@gmail.com>
2023-04-01 20:56:04 +0200

    add user class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class main {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Q1
+        String sentence = "This is Activity 4; it's about using Collections Framework and Functional Programming. ";
+        List<String> arrayList = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+        Collections.sort(arrayList);
+        System.out.println("ArrayList: "+arrayList);
+
+        //Q2
+        Set<String> hashSet = new HashSet<>(arrayList);
+        System.out.println("HashSet: "+hashSet + "\n");
+
+        //Q3
+        String[][] cars = {{"AUDI", "4"}, {"KIA", "10"}, {"BMW", "5"}, {"FORD", "2"}};
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (String[] car : cars) {
+            hashMap.put(car[0], Integer.parseInt(car[1]));
+        }
+        System.out.println("HashMap: "+hashMap);
+
+        //Q4
+        int result = hashMap.values().stream().reduce(1, (x, y) -> x * y);
+        System.out.println("The multiply of the HashMap values = "+result+"\n");
+
+    }
 }
---
+++ src/User.java
@@ -0,0 +1,65 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+     private String  name , password  , email;
+    private int id ,age , phone;
+
+    public User(int id, String name, String password, int age, int phone, String email ) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.id = id;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
+
